4fe3aa696a74ea74c36030d7a9f610b1bdfe42de
ASM-Monirul-Islam <monirul.01401411046@gmail.com>
2025-09-03 12:20:00 +0600

    updated covid

--- 00_CodeRunner.cpp
+++ 00_CodeRunner.cpp
@@ -1,54 +1,72 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void DFS(int node, vector<vector<int>>&adj, vector<bool>& visited, stack<int>&s) {
-	visited[node]=true;
-	for(auto i:adj[node]) {
-		if(!visited[i]) {
-			DFS(i, adj, visited, s);
-		}
-	}
-	s.push(node);
+bool valid(int i, int j, int r, int c)
+{
+	return i >= 0 and i < r and j >= 0 and j < c;
 }
 
-vector<int>TopologicalSort(int v, vector<vector<int>>&adj) {
-	stack<int>s;
-	vector<bool>visited(v, false);
-	for(int i=0; i<v; i++) {
-		if(!visited[i]) {
-			DFS(i, adj, visited, s);
+int CovidCheck(int r, int c, vector<vector<int>> &adj, queue<pair<int, int>> &q)
+{
+	int time = 0;
+	while (!q.empty())
+	{
+		time++;
+		int s = q.size();
+		while (s--)
+		{
+			int i = q.front().first;
+			int j = q.front().second;
+			q.pop();
+			int row[4] = {-1, 1, 0, 0};
+			int col[4] = {0, 0, -1, 1};
+			for (int k = 0; k < 4; k++)
+			{
+				if (valid(i + row[k], j + col[k], r, c) and adj[i + row[k]][j + col[k]] == 1)
+				{
+					adj[i + row[k]][j + col[k]] = 2;
+					q.push({i + row[k], j + col[k]});
+				}
+			}
 		}
 	}
-	vector<int>ans;
-	while(!s.empty()) {
-		ans.push_back(s.top());
-		s.pop();
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			if (adj[i][j] == 1)
+			{
+				return -1;
+			}
+		}
 	}
-	return ans;
+	return time-1;
 }
 
-int main() {
-	int v, e;
-	char n, x, y;
-	map<char, int>node;
-	map<int, char>inverse_node;
-	cout<<"Enter the vertex and edges number: ";
-	cin>>v>>e;
-	cout<<"Enter the name of the nodes: (e.g.: A, B, C, ...): ";
-	for(int i=0; i<v; i++) {
-		cin>>n;
-		node[n]=i;
-		inverse_node[i]=n;
+int main()
+{
+	int r, c;
+	cout << "Enter the number of row and column: ";
+	cin >> r >> c;
+	vector<vector<int>> adj(r, vector<int>(c));
+	queue<pair<int, int>> q;
+	cout << "Enter the nodes: " << endl;
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			cin >> adj[i][j];
+			if (adj[i][j] == 2)
+			{
+				q.push({i, j});
+			}
+		}
 	}
-	vector<vector<int>>adj(v);
-	for(int i=0; i<e; i++) {
-		cout<<"Enter the starting node and the ending node: ";
-		cin>>x>>y;
-		adj[node[x]].push_back(node[y]);
-	}
-	vector<int>ans = TopologicalSort(v, adj);
-	for(auto i: ans) {
-		cout<<inverse_node[i]<<" ";
+	int ans = CovidCheck(r, c, adj, q);
+	if(ans == -1) {
+		cout<<"Whole hospital is not affected by Covid 19"<<endl;
+	} else {
+		cout<<"It will take "<<ans<<" units of time to spread Covid 19"<<endl;
 	}
 	return 0;
 }
